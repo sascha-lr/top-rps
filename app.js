de65f06662e1719b25078b7ca5bb4969c9d47ed6
sascha-lr <174216621+sascha-lr@users.noreply.github.com>
2026-02-02 10:04:24 +0100

    Add better practices

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ function getRandomInt() {
   return Math.floor(Math.random() * 3);
 }
 
+function capitalizeFirst(string) {
+    return string.at(0).toUpperCase() + string.slice(1);
+}
+
 function getComputerChoice () {
-    let randomNumber = getRandomInt();
+    const randomNumber = getRandomInt();
     if (randomNumber === 0) {
         return "rock";
     } else if (randomNumber === 1) {
@@ -16,7 +20,7 @@ function getComputerChoice () {
 }
 
 function getHumanChoice () {
-    let humanChoice = prompt("Type either Rock, Paper or Scissors","f.e.: Rock").toLowerCase();
+    const humanChoice = prompt("Type either Rock, Paper or Scissors","f.e.: Rock").toLowerCase();
     if (!(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors")) {
         alert("You have to type either Rock, Paper or Scissors and respect proper grammar! Trying again...");
         return getHumanChoice();
@@ -31,33 +35,25 @@ function playGame () {
     let computerScore = 0;
     
     function playRound (humanChoice, computerChoice) {
+        
+        humanChoice = capitalizeFirst(humanChoice);
+        computerChoice = capitalizeFirst(computerChoice);
+
         if (humanChoice === computerChoice) {
             alert(`Tie! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
+        } else if (humanChoice === "Rock" && computerChoice === "Paper" || humanChoice === "Scissors" && computerChoice === "Rock" || humanChoice === "Paper" && computerChoice === "Scissors") {
             computerScore += 1;
-            alert(`You lose! Paper beats Rock! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore += 1;
-            alert(`You win! Paper beats Rock! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore += 1;
-            alert(`You win! Rock beats Scissors! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            computerScore += 1;
-            alert(`You lose! Rock beats Scissors! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore += 1;
-            alert(`You lose! Scissors beat Paper! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
+            alert(`You lose! ${computerChoice} beats ${humanChoice}! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
         } else {
             humanScore += 1;
-            alert(`You win! Scissors beat Paper! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
+            alert(`You win! ${humanChoice} beats ${computerChoice}! Current Human Score: ${humanScore}. Current Computer Score: ${computerScore}`);
         }
     }
 
     playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice()); 
     playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice()); 
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
     playRound(getHumanChoice(), getComputerChoice());
 }
 
